ee571d3e9804acf19d1d660483bb7ed1efd2da73
secret <2691475984@qq.com>
2020-12-02 21:56:18 +0800

    Add files via upload

---
+++ java/cn/mcbbswiki/qwerty5238/block/BlockNoobX.java
@@ -0,0 +1,14 @@
+package cn.mcbbswiki.qwerty5238.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
+
+public class BlockNoobX extends Block {
+    public BlockNoobX(){
+        super(Properties.create(Material.ROCK)
+                .hardnessAndResistance(30)
+                .harvestTool(ToolType.get("pickaxe"))
+                .harvestLevel(2));
+    }
+}
