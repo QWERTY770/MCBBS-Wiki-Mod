a601d6c1f9bb2af9266972b4b7e0866f4098f5b0
secret <2691475984@qq.com>
2021-01-20 18:07:18 +0800

    Delete McbbsWikiNormalDimension.java

--- src/main/java/cn/mcbbswiki/qwerty5238/world/dimension/McbbsWikiNormalDimension.java
+++
@@ -1,9 +0,0 @@
-package cn.mcbbswiki.qwerty5238.world.dimension;
-
-import cn.mcbbswiki.qwerty5238.util.Uncompleted;
-
-@Uncompleted
-public class McbbsWikiNormalDimension {
-    // uncompleted
-    // use JSON instead now
-}
